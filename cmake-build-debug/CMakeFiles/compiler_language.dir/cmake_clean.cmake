01dcbcf154ce3b23b39d7f2273a28e112db15bc1
Shulgan-Roman <romashulgan@icloud.com>
2024-12-26 02:01:12 +0300

    commit

---
+++ cmake-build-debug/CMakeFiles/compiler_language.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compiler_language.dir/main.cpp.o"
+  "CMakeFiles/compiler_language.dir/main.cpp.o.d"
+  "compiler_language"
+  "compiler_language.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compiler_language.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
